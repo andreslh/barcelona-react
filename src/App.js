4f8d7fe6d723b310677c4eb4d1b7eda7fe60e139
andreslh <andreslh90@gmail.com>
2021-01-28 15:20:09 +0100

    chore: router added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
 import { Counter } from './features/counter/Counter';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 function App() {
-  return <Counter />;
+  const [anchorEl, setAnchorEl] = useState(null);
+  const classes = useStyles();
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <Router>
+      <Container>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Barcelona
+            </Typography>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+              aria-controls="header-menu"
+              aria-haspopup="true"
+              onClick={handleClick}
+              data-testid="header-menu-icon"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="header-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>
+                <Link to="/">Mesas</Link>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <Link to="/counter">Comidas</Link>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <Link to="/">Bebidas</Link>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <Link to="/">Pedidos anteriores</Link>
+              </MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+
+        <Switch>
+          <Route path="/counter">
+            <Counter />
+          </Route>
+          <Route path="/">Home</Route>
+        </Switch>
+      </Container>
+    </Router>
+  );
 }
 
 export default App;
