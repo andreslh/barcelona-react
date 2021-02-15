273c659b8b38c86558391263db67bd0d5f385338
andreslh <andreslh90@gmail.com>
2021-02-15 11:27:57 +0100

    feat: addproducts table header removed

--- src/features/tables/AddProducts/Subcategories.js
+++ src/features/tables/AddProducts/Subcategories.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Grid from '@material-ui/core/Grid';
 
 import { SubcategoryProducts } from './SubcategoryProducts';
@@ -16,13 +13,6 @@ export function Subcategories({ category }) {
       <Grid data-testid="subcategory" item xs={12} md={6} key={subcategory.id}>
         <SubcategoryTitle>{subcategory.name}</SubcategoryTitle>
         <Table aria-label="active tables">
-          <TableHead>
-            <TableRow>
-              <TableCell align="left">Producto</TableCell>
-              <TableCell align="left">Cantidad</TableCell>
-            </TableRow>
-          </TableHead>
-
           <TableBody>
             <SubcategoryProducts subcategory={subcategory} />
           </TableBody>
