409e5ce75ac174fe6ed07e9d8370dc5dc768b595
Andrés Ledesma Higonet <andreslh90@gmail.com>
2022-01-08 21:57:57 -0300

    feat(TablesList): posibilidad de colapsar por mozo

--- src/features/tables/TablesList.js
+++ src/features/tables/TablesList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Table from '@material-ui/core/Table';
@@ -8,12 +8,14 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import { Grid } from '@material-ui/core';
 import styled from 'styled-components';
 
+import { ACTIVE_TABLE } from '../../app/routes';
 import { selectActive, selectTables } from './tablesSlice';
 
 import './TablesList.css';
-import { ACTIVE_TABLE } from '../../app/routes';
 
 const TableLink = styled(Link)`
   display: flex;
@@ -24,20 +26,38 @@ const TableLink = styled(Link)`
 export default function TablesList() {
   const tablesByWaiter = useSelector(selectTables);
   const active = useSelector(selectActive);
+  const [tablesVisibility, setTablesVisibility] = useState([]);
 
   const tablesList = [];
-  tablesByWaiter.forEach((waiter) => {
+  tablesByWaiter.forEach((waiter, index) => {
+    tablesVisibility.push(true);
     tablesList.push(
       <Table key={waiter.id} aria-label='active tables'>
         <TableHead classes={{ root: 'table-header' }}>
           <TableRow>
-            <TableCell align='left'>
+            <TableCell children={<Grid
+              container
+              justify="space-between"
+              classes={{ root: 'pointer table-header' }}
+              onClick={() => {
+                const newTablesVisibility = [...tablesVisibility];
+                newTablesVisibility[index] = !newTablesVisibility[index];
+                setTablesVisibility(newTablesVisibility);
+              }}
+            >
               <b>{waiter.name}</b>
+              <ChevronRightIcon
+                fontSize="small"
+                classes={{ root: tablesVisibility[index] ? 'chevron-down' : '' }}
+              />
+            </Grid>}>
             </TableCell>
           </TableRow>
         </TableHead>
 
-        <TableBody>
+        <TableBody classes={{
+          root: tablesVisibility[index] ? 'table-list-visible' : 'table-list-hidden'    
+        }}>
           {waiter.Tables.map((table) => (
             <TableRow
               key={table.id}
